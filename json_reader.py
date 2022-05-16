1fadbffa17006f852072939bb34e1877c6eb620c
PeterSelie420 <martijntjuuu@gmail.com>
2022-05-16 13:58:55 +0200

    Added scene extractor and rotation + translation correction

--- json_reader.py
+++ json_reader.py
@@ -13,10 +13,13 @@ class JsonReader:
 
         self.cur_scene = 'According to all known laws of aviation, there is no way a bee should be able to fly'
         # Read JSON files
+        print("Parsing json ego pose")
         with open(ego_pose, 'r') as fcc_file:
             self.ego_pose_read = json.load(fcc_file)
+        print("Parsing json sample data")
         with open(sample_data, 'r') as fcc_file:
             self.sample_data_read = json.load(fcc_file)
+        print("Parsing json sample")
         with open(sample, 'r') as fcc_file:
             self.sample_read = json.load(fcc_file)
 
@@ -36,4 +39,31 @@ class JsonReader:
                 for gigahuts in self.ego_pose_read:
                     if gigahuts['token'] == huts['ego_pose_token']:
                         print(gigahuts['translation'])
-                        return gigahuts['translation']
+                        print(gigahuts['rotation'])
+                        return gigahuts['translation'], gigahuts['rotation']
+
+    def get_scene(self, index: int, file_locations):
+        start = 0
+        end = 0
+        f = 0
+        i = 0
+        cur_scene = "According to all known laws of aviation, there is no way a bee should be able to fly"
+        # For file in file location, check scene, and iterate index for each new scene found
+        for file in file_locations:
+            file_location = file.split(
+                "/home/carpc/PycharmProjects/Lidar_to_Radar/dataset/nu_scenes/v1.0-trainval01_blobs/"
+            )[1]
+            for huts in self.sample_data_read:
+                if huts['filename'] == file_location:
+                    for megahuts in self.sample_read:
+                        if megahuts['token'] == huts['sample_token']:
+                            if megahuts['scene_token'] != cur_scene:
+                                cur_scene = megahuts['scene_token']
+                                i = i + 1
+                                start = end
+                                end = f
+                                if i >= index + 1:
+                                    return start, end
+                                print("New scene detected!")
+            f = f + 1
+        print("Scene not found ;(")
--- visualize_nu_scenes.py
+++ visualize_nu_scenes.py
@@ -1,7 +1,8 @@
 import open3d as o3d
 
-# Lidar
 import data_nu_scenes
+import json_reader
+import numpy as np
 
 
 def visualize_with_viewpoint(cloud):
@@ -12,6 +13,7 @@ def visualize_with_viewpoint(cloud):
 
 
 data_nu_scenes = data_nu_scenes.DataNuScenes()
+json = json_reader.JsonReader()
 start = 0
 max_lidar_points = 0
 max_radar_points = 0
@@ -19,22 +21,30 @@ max_radar_points = 0
 vis = o3d.visualization.Visualizer()
 vis.create_window(width=500, height=500)
 
-for f in range(start, 1):
-    o3d_pcd_lidar = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(data_nu_scenes.get_lidar(f, max_lidar_points))) \
-        .paint_uniform_color([0, 0, 0])
-    o3d_pcd_lidar_2 = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(data_nu_scenes.get_lidar(f + 1, max_lidar_points))) \
-        .paint_uniform_color([0, 0, 0])
-    o3d_pcd_lidar_3 = o3d.geometry.PointCloud(
-        o3d.utility.Vector3dVector(data_nu_scenes.get_lidar(f + 2, max_lidar_points))) \
-        .paint_uniform_color([0, 0, 0])
-    o3d_pcd_lidar_4 = o3d.geometry.PointCloud(
-        o3d.utility.Vector3dVector(data_nu_scenes.get_lidar(f + 3, max_lidar_points))) \
-        .paint_uniform_color([0, 0, 0])
-    o3d_pcd_lidar_5 = o3d.geometry.PointCloud(
-        o3d.utility.Vector3dVector(data_nu_scenes.get_lidar(f + 4, max_lidar_points))) \
-        .paint_uniform_color([0, 0, 0])
+# For each pointcloud in dataset
+# Check which scene, if same scene, get translation and add to cloud geometry
+# If scene differ, start visualization
+cur_scene = 'According to all known laws of aviation, there is no way a bee should be able to fly'
+initial_translation = 0
+o3d_pcd_lidar_comb = None
+list_of_lidar = data_nu_scenes.list_of_files_lidar
+print(json.get_scene(4, list_of_lidar))
+
+for f in range(119, 158):
+    o3d_pcd_lidar = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(data_nu_scenes.get_lidar(f, max_lidar_points)))
+    translation, rotation = json.get_translation(data_nu_scenes.get_lidar_path(f))
+    if initial_translation == 0:
+        initial_translation = translation
+    translation = np.subtract(translation, initial_translation)
+    R = o3d.geometry.get_rotation_matrix_from_quaternion(rotation)
+    o3d_pcd_lidar_translated = o3d_pcd_lidar.translate((translation[1], translation[0], translation[2]))
+    if o3d_pcd_lidar_comb is None:
+        o3d_pcd_lidar_comb = o3d_pcd_lidar_translated
+    else:
+        o3d_pcd_lidar_comb += o3d_pcd_lidar_translated
+
     # Radar
     o3d_pcd_radar_comb = o3d.geometry.PointCloud(data_nu_scenes.get_radar(f, max_radar_points)).paint_uniform_color(
         [0, 0, 0])
-    visualize_with_viewpoint(o3d_pcd_lidar + o3d_pcd_lidar_2 + o3d_pcd_lidar_3 + o3d_pcd_lidar_4 + o3d_pcd_lidar_5)
-    gigahuts = 100
+visualize_with_viewpoint(o3d_pcd_lidar_comb)
+gigahuts = 100
